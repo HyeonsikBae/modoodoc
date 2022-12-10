9446c9ed23f86b66f03468582a2b9ae5003facce
Hyeonsik Bae <baehyeonsik@gmail.com>
2022-12-10 12:08:46 +0900

    style: add theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+const colors = {
+  black: "#000000",
+  white: "#ffffff",
+  purple: "#4d1192",
+  lightPurple: "ece3f8",
+  lightGray: "f8f8f8",
+  gray: "9b9b9b",
+  darkGray: "494949",
+};
+
+const theme = {
+  colors,
+};
+
+export default theme;
