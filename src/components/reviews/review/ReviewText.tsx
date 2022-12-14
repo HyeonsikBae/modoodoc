1e021cf18534c0210417f61662438f804d000a5d
Hyeonsik Bae <baehyeonsik@gmail.com>
2022-12-14 14:55:51 +0900

    feat: text folding

--- src/components/reviews/review/ReviewText.tsx
+++ src/components/reviews/review/ReviewText.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Span from "../../utils/Span";
 
 interface ReviewTextProps {
@@ -6,9 +6,30 @@ interface ReviewTextProps {
 }
 
 const ReviewText = (props: ReviewTextProps): JSX.Element => {
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+
   const { text } = props;
   const len = text.length;
-  return len <= 100 ? <Span>{text}</Span> : <Span>{text.slice(0, 85)}...</Span>;
+
+  const toggle = (): void => {
+    setIsClicked((isClicked) => !isClicked);
+  };
+
+  return len <= 100 ? (
+    <Span fontSize="0.8rem">{text}</Span>
+  ) : (
+    <Span fontSize="0.8rem">
+      {isClicked === true ? `${text}` : `${text.slice(0, 85)}`}
+      <button
+        style={{ display: "inline", padding: "0" }}
+        onClick={(): void => toggle()}
+      >
+        <Span fontSize="0.8rem" fontWeight="bold">
+          {isClicked === true ? `접기` : `...더보기`}
+        </Span>
+      </button>
+    </Span>
+  );
 };
 
 export default ReviewText;
